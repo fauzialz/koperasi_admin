beccee4bf640128e75f1c344bd3a70e3100023cd
Fauzi Al Aziz <42629134+ekalayabas@users.noreply.github.com>
2019-02-05 01:47:42 +0700

    Styling Login view (experimenting).

---
+++ src/views/login/Login.js
@@ -0,0 +1,19 @@
+import React from 'react' 
+import './Login.css'
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div className="App-base">
+                <div className="App-container shadow">
+                    <div className="title">
+                        Sign In 
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login
+
