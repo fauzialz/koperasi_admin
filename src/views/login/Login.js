8dbfd0353b4fd798db1bfafe9b9db2e015203b91
Fauzi Al Aziz <42629134+ekalayabas@users.noreply.github.com>
2019-02-05 23:34:36 +0700

    Trying material ui styling framework.

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,14 +1,46 @@
 import React from 'react' 
+import TextField from '@material-ui/core/TextField'
 import './Login.css'
 
 class Login extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            username : '',
+            password : ''
+        }
+    }
+
+    textChange = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
     render() {
         return (
-            <div className="App-base">
-                <div className="App-container shadow">
-                    <div className="title">
+            <div className="Login-base">
+                <div className="Login-container">
+                    <div className="Login-title">
                         Sign In 
                     </div>
+
+                    <form>
+                        {/* <input 
+                            type="text"
+                            name= "username"
+                            placeholder="Username"
+                            
+                            value={this.state.username}
+                            onChange={this.textChange}
+                        /> */}
+                        <TextField
+                            label="Username"
+                            name="username"
+                            margin="dense"
+                            fullWidth
+                            value={this.state.username}
+                            onChange={this.textChange}
+                        />
+                    </form>
                 </div>
             </div>
         )
