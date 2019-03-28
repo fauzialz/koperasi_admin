e4ba10fc9844b84300b379717c1e56d024c1d23f
Fauzi Al Aziz <42629134+ekalayabas@users.noreply.github.com>
2019-03-29 02:07:18 +0700

    Fix bug in NavBar when normal and edit session.

--- src/views/dashboard/navbar/Navbar.js
+++ src/views/dashboard/navbar/Navbar.js
@@ -18,6 +18,7 @@ class Navbar extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            historyId : 0,
             navList : [],
             navObj  : {},
             editSession : false,
@@ -28,57 +29,58 @@ class Navbar extends React.Component {
             title: 'Setting Mode'
         }
     }
-
+    
+    /*  Switch loading on or off. */
     loadingSwitch = () => {
         this.setState({loading : !this.state.loading})
     }
-
-    editSessionSwitch = () => {
-        this.setState({
-            editSession: !this.state.editSession,
-        })
-        if(this.state.title === "Setting Mode") {
-            setTimeout(() => {
-                this.setState({
-                    title: "Setti..."
-                })
-            }, 100);
-        }else{
-            setTimeout(() => {
-                this.setState({
-                    title: "Setting Mode"
-                })
-            }, 400);
-        }
-    }
-
-    editSessionButton = () => {
-        if(this.state.editSession === false){
-            this.setState({openAuthModal:true})
-        }else{
-            this.editSessionSwitch()
-        }
-    }
-
-    tileHendler = (id) => {
-        if(this.state.editSession){
-            //edit menu show modal
-            this.editModalHandler(id)
-        }else{
-            //open menu
+    
+    /*  Decide the Tiles to work normal or become button to edit navlist. */
+    tileHandler = (id) => {
+        if(!this.state.editSession){
+            /* open menu */
             this.activeTileHandler(id)
+        }else{
+            /* edit menu show modal */
+            this.openTileEdit(id)
         }
     }
-    unclickedHandler = (id) => {
-        let hotNavList = this.state.navList
+    
+    /*  Active a Neutral Tiles or Child Tiles by detecting navlist and
+        param id. If navObj.Id equal id, inject true into navObj.Active,
+        or navObj.Clicked if navObj have children (Parent Tiles). */
+    activeTileHandler = (id, hotNavList = this.state.navList) => {
+        debugger
+        let parentClicked = this.parentClickChecker(id)
         for(var i = 0; i< hotNavList.length; i++) {
-            if(hotNavList[i].Id !== id) {
-                hotNavList[i].Clicked = false
+            if(hotNavList[i].Children.length > 0 && hotNavList[i].Id === id) {
+                hotNavList[i].Clicked = !hotNavList[i].Clicked
+                hotNavList = this.unclickedHandler(id)
+            }else{
+                if(hotNavList[i].Id === id) {
+                    hotNavList[i].Active = true
+                    this.setState({historyId : id})
+                    //do something base with route
+                }else if(!parentClicked) {
+                    hotNavList[i].Active = false
+                }
+            }
+            if(hotNavList[i].Children.length > 0){
+                for(var j = 0; j< hotNavList[i].Children.length; j++) {
+                    if( hotNavList[i].Children[j].Id === id)  {
+                        hotNavList[i].Children[j].Active = true
+                        this.setState({historyId : id})
+                        //do something base with route
+                    }else if(!parentClicked){
+                        hotNavList[i].Children[j].Active = false
+                    } 
+                }
             }
         }
-        return hotNavList
+        this.setState({navList : hotNavList})
     }
-
+    
+    /*  Detect if along the Tiles there is a Parent Tile that clicked. */
     parentClickChecker = (id) => {
         let hotNavList = this.state.navList
         let parentClicked = false
@@ -89,36 +91,64 @@ class Navbar extends React.Component {
         }
         return parentClicked
     }
-
-    activeTileHandler = (id) => {
+    
+    /*  Make non-clicked Parent Tiles close. */
+    unclickedHandler = (id = 10000) => {
         let hotNavList = this.state.navList
-        let parentClicked = this.parentClickChecker(id)
         for(var i = 0; i< hotNavList.length; i++) {
-            if(hotNavList[i].Children.length > 0 && hotNavList[i].Id === id) {
-                hotNavList[i].Clicked = !hotNavList[i].Clicked
-                hotNavList = this.unclickedHandler(id)
-            }else{
-                if(hotNavList[i].Id === id) {
-                    hotNavList[i].Active = true
-                    //do something base with route
-                }else if(!parentClicked) {
-                    hotNavList[i].Active = false
-                }
-            }
-            if(hotNavList[i].Children.length > 0){
-                for(var j = 0; j< hotNavList[i].Children.length; j++) {
-                    if( hotNavList[i].Children[j].Id === id)  {
-                        hotNavList[i].Children[j].Active = true
-                        //do something base with route
-                    }else if(!parentClicked){
-                        hotNavList[i].Children[j].Active = false
-                    } 
-                }
+            if(hotNavList[i].Id !== id) {
+                hotNavList[i].Clicked = false
             }
         }
-        this.setState({navList : hotNavList})
+        return hotNavList
+    }
+    
+    /*  onClick function for Setting Mode. Decide if Auth Modal must
+        show or not. Open Auth Modal if Setting Mode is off, call a function
+        to turn off the Setting Mode off if it already on. */
+    editSessionButton = () => {
+        if(this.state.editSession === false){
+            this.setState({openAuthModal:true})
+        }else{
+            this.editSessionSwitch()
+        }
+    }
+    
+    /*  Switch for turn Setting Mode on (with all the atribute, include call
+        a function for open all Parent Tiles.) or off (with all the atribute,
+        include call a function to make non-clicked Prent Tiles Close). */
+    editSessionSwitch = () => {
+        this.setState({
+            editSession: !this.state.editSession,
+        })
+        if(this.state.title === "Setting Mode") {
+            setTimeout(() => {
+                this.setState({
+                    title: "Setti...",
+                    navList: this.clickedAllHandler()
+                })
+            }, 100);
+        }else{
+            setTimeout(() => {
+                this.setState({
+                    title: "Setting Mode",
+                    navList: this.unclickedHandler()
+                })
+            }, 400);
+        }
     }
 
+    /*  Make all Parent Tiles open */
+    clickedAllHandler = (hotNavList = this.state.navList) => {
+        for(var i = 0; i< hotNavList.length; i++) {
+            if(hotNavList[i].Children.length > 0) {
+                hotNavList[i].Clicked = true
+            }
+        }
+        return hotNavList
+    }
+
+    /*  Fetch navList data from API and update it in localStorage. */
     getNavigationList = () => {
         this.loadingSwitch()
         HelperHttp.request(ConfigApi.ROUTE.GET_MENU, ConfigApi.METHODS.GET, {},
@@ -127,16 +157,23 @@ class Navbar extends React.Component {
                 if(success){
                     let list = HelperModData.pushObjBulk(response.Result,'Active',false)
                     list = HelperModData.pushObjBulk(list,'Clicked',false)
-                    this.setState({
-                        navList : list
-                    })
-                    localStorage.setItem(ConfigLocal.LOCSTORE.Navbar,JSON.stringify(list))
+                    if(this.state.editSession) {
+                        list = this.clickedAllHandler(list)
+                        this.activeTileHandler(this.state.historyId, list)
+                    }else{
+                        this.setState({
+                            navList : list
+                        })
+                        localStorage.setItem(ConfigLocal.LOCSTORE.Navbar,JSON.stringify(list))
+                    }
                 }
             }
         )
     }
 
-    editModalHandler = (id) => {
+    /*  Open modal for edit tile. Fetch navObj data
+        from id to get eTag, then open modal. */
+    openTileEdit = (id) => {
         this.loadingSwitch()
         let route = ConfigApi.ROUTE.MENU + '/' +id
         HelperHttp.request(route, ConfigApi.METHODS.GET, {}, 
@@ -152,6 +189,7 @@ class Navbar extends React.Component {
         )
     }
 
+    /*  To close all modal. */
     onClose = () => {
         this.setState({
             openAuthModal: false,
@@ -159,6 +197,8 @@ class Navbar extends React.Component {
         })
     }
 
+    /*  LifeCircle. Detect if navlist has been stored in LocalStorege or not.
+        If yes, use it immediately. If not, call function to fect the data. */
     componentDidMount() {
         let list = JSON.parse(localStorage.getItem(ConfigLocal.LOCSTORE.Navbar))
         if(list != null){
@@ -197,7 +237,7 @@ class Navbar extends React.Component {
                 <div className={this.props.open ? "open-navbar": "close-navbar"}> 
                         <div className="navbar-base">
                             <div className= "navbar-overflow-superadmin">
-                                <NavbarTiles navList={navList} onClick={this.tileHendler} />
+                                <NavbarTiles navList={navList} onClick={this.tileHandler} />
                             </div>
                             <div className= "edit-tile">
                             <div className={this.state.editSession? "navbar-add-on": "navbar-add-off"}>
