73eceef14b29139107dc12c69c9a94b042df23a2
Fauzi Al Aziz <42629134+ekalayabas@users.noreply.github.com>
2019-05-17 00:55:44 +0700

    Add content loading mechanism and table holder.

--- src/views/contents/store/Store.js
+++ src/views/contents/store/Store.js
@@ -2,18 +2,44 @@ import React from 'react'
 import Dummy from '../dummy';
 import HelperHttp from '../../../helper/HelperHttp';
 import ConfigApi from '../../../config/ConfigApi';
+import './Store.scss'
+import ConfigLocal from '../../../config/ConfigLocal';
 
 class Store extends React.Component {
-
+    state = {
+        contentData : {},
+        contentLoading : true,
+        contentProps : {
+            title : 'Store'
+        }
+    }
 
     componentDidMount() {
-        HelperHttp.get(ConfigApi.ROUTE.STORE, (res) => {
-            alert(res.message)
+        this.setState({contentLoading : true})
+        debugger
+        HelperHttp.get(ConfigApi.ROUTE.STORE, (res)  => {
+            this.setState({
+                contentData : res,
+                contentLoading : false
+            })
         })
     }
 
     render () {
-        return <Dummy />
+        const { contentProps } = this.state
+        return (
+            <React.Fragment>
+                { this.state.contentLoading ?
+                    <Dummy />:
+                    <div className="content-wrapper">
+                        <div className="content-title">{contentProps.title}</div>
+                        <div className="table-holder">
+                            {ConfigLocal.LOREMIPSUM}
+                        </div>
+                    </div>
+                }
+            </React.Fragment>
+        )
     }
 }
 
