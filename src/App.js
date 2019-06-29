22aa88d91e29c1aee21e6839c33b72821ac2289b
Fauzi Al Aziz <42629134+ekalayabas@users.noreply.github.com>
2019-06-30 04:12:15 +0700

    Loading Mini for pagination indicator.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { RouterTierOne } from './Router';
 import { ProviderScope, AppContext } from './global';
-import Loading from './components/loading';
+import { LoadingBig, LoadingMini } from './components/loading';
 
 class App extends Component {
   render() {
@@ -13,7 +13,8 @@ class App extends Component {
           <AppContext.Consumer>
             {context => (
               <React.Fragment>
-                {context.loading && <Loading />}
+                {context.loading && <LoadingBig />}
+                {context.loadingMini && <LoadingMini />}
               </React.Fragment>
             )}
           </AppContext.Consumer>
